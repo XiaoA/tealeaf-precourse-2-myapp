a3821bda782c79650639cc6d994e9e8ad200bd2e
Andrew Buckingham <abuckingham99@gmail.com>
2015-03-11 23:27:25 -0500

    Update associations in group.rb and user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312040244) do
+ActiveRecord::Schema.define(version: 20150312041907) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 20150312040244) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "group_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id"
+  add_index "memberships", ["user_id"], name: "index_memberships_on_user_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "author"
